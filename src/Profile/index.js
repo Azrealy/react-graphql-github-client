ded5922862563df2a6260d9dd8a366fe3c2dd487
Azrealy <fangkaihang1992@yahoo.co.jp>
2018-09-24 16:20:43 +0900

    Implement pagination of graphql client

--- src/Profile/index.js
+++ src/Profile/index.js
@@ -7,36 +7,46 @@ import { graphql } from 'react-apollo';
 import Loading from '../Loading';
 
 const GET_REPOSITORIES_OF_CURRENT_USER = gql`
-  {
+query($cursor: String) {
     viewer {
       repositories(
         first: 5
         orderBy: { direction: DESC, field: STARGAZERS }
+        after: $cursor
       ) {
         edges {
           node {
             ...repository
           }
         }
+        pageInfo {
+          endCursor
+          hasNextPage
+        }
       }
     }
   }
+
   ${REPOSITORY_FRAGMENT}
 `;
 
-const Profile = ({ data, loading, error }) => {
+const Profile = () => (
+  <Query query={GET_REPOSITORIES_OF_CURRENT_USER} notifyOnNetworkStatusChange={true}>
+  {({ data, loading, error, fetchMore }) => {
   if (error) {
     return <ErrorMessage error={error} />;
   }
 
   const { viewer } = data;
 
-  if (loading || !viewer) {
+  if (loading && !viewer) {
     return <Loading />;
   }
 
-  return <RepositoryList repositories={viewer.repositories} />;
-};
+      return <RepositoryList loading={loading} repositories={viewer.repositories} fetchMore={fetchMore}/>
+  }}
+  </Query>
+);
 
 
 export default graphql(GET_REPOSITORIES_OF_CURRENT_USER)(Profile);
--- src/Repository/RepositoryItem/index.js
+++ src/Repository/RepositoryItem/index.js
@@ -17,6 +17,61 @@ const STAT_REPOSITORY = gql`
   }
 `
 
+const WATCH_REPOSITORY = gql`
+  mutation ($id: ID!, $viewerSubscription: SubscriptionState!) {
+    updateSubscription(
+      input: { state: $viewerSubscription, subscribableId: $id }
+    ) {
+      subscribable {
+        id
+        viewerSubscription
+      }
+    }
+  }
+`;
+
+const VIEWER_SUBSCRIPTIONS = {
+  SUBSCRIBED: 'SUBSCRIBED',
+  UNSUBSCRIBED: 'UNSUBSCRIBED',
+};
+
+const isWatch = viewerSubscription =>
+  viewerSubscription === VIEWER_SUBSCRIPTIONS.SUBSCRIBED;
+
+const updateWatch = (
+  client,
+  {
+    data: {
+      updateSubscription: {
+        subscribable: { id, viewerSubscription },
+      },
+    },
+  },
+) => {
+  const repository = client.readFragment({
+    id: `Repository:${id}`,
+    fragment: REPOSITORY_FRAGMENT,
+  });
+
+  let { totalCount } = repository.watchers;
+  totalCount =
+    viewerSubscription === VIEWER_SUBSCRIPTIONS.SUBSCRIBED
+      ? totalCount + 1
+      : totalCount - 1;
+
+  client.writeFragment({
+    id: `Repository:${id}`,
+    fragment: REPOSITORY_FRAGMENT,
+    data: {
+      ...repository,
+      watchers: {
+        ...repository.watchers,
+        totalCount,
+      },
+    },
+  });
+};
+
 const updateAddStar = (
   client,
     { data: { addStar: { starrable: { id } } } },) => {
@@ -76,6 +131,38 @@ const RepositoryItem = ({
             <span>he </span>
           )
           }
+          <Mutation
+            mutation={WATCH_REPOSITORY}
+            variables={{
+              id,
+              viewerSubscription: isWatch(viewerSubscription)
+                ? VIEWER_SUBSCRIPTIONS.UNSUBSCRIBED
+                : VIEWER_SUBSCRIPTIONS.SUBSCRIBED,
+            }}
+            optimisticResponse={{
+              updateSubscription: {
+                __typename: 'Mutation',
+                subscribable: {
+                  __typename: 'Repository',
+                  id,
+                  viewerSubscription: isWatch(viewerSubscription)
+                    ? VIEWER_SUBSCRIPTIONS.UNSUBSCRIBED
+                    : VIEWER_SUBSCRIPTIONS.SUBSCRIBED,
+                },
+              },
+            }}
+            update={updateWatch}
+          >
+            {(updateSubscription, { data, loading, error }) => (
+              <Button
+                className="RepositoryItem-title-action"
+                onClick={updateSubscription}
+              >
+                {watchers.totalCount}{' '}
+                {isWatch(viewerSubscription) ? 'Unwatch' : 'Watch'}
+              </Button>
+            )}
+          </Mutation>
       </div>
       <div className="RepositoryItem-title-action">
         {stargazers.totalCount} Stars
